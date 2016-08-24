587ca0d9eb1355b816988868d48e8533838a23ae
dionelrodriguez <info@apt3k.com>
2016-08-24 17:14:44 -0400

    - Removed the dashboard (v23).

--- app/build/generated/source/r/debug/info/androidhive/loginandregistration/R.java
+++ app/build/generated/source/r/debug/info/androidhive/loginandregistration/R.java
@@ -2708,22 +2708,21 @@ containing a value of this type.
         public static final int design_navigation_item_subheader=0x7f04002a;
         public static final int design_navigation_menu=0x7f04002b;
         public static final int design_navigation_menu_item=0x7f04002c;
-        public static final int drawerlayout=0x7f04002d;
-        public static final int na_drawer_profile_image=0x7f04002e;
-        public static final int nav_header=0x7f04002f;
-        public static final int notification_media_action=0x7f040030;
-        public static final int notification_media_cancel_action=0x7f040031;
-        public static final int notification_template_big_media=0x7f040032;
-        public static final int notification_template_big_media_narrow=0x7f040033;
-        public static final int notification_template_lines=0x7f040034;
-        public static final int notification_template_media=0x7f040035;
-        public static final int notification_template_part_chronometer=0x7f040036;
-        public static final int notification_template_part_time=0x7f040037;
-        public static final int select_dialog_item_material=0x7f040038;
-        public static final int select_dialog_multichoice_material=0x7f040039;
-        public static final int select_dialog_singlechoice_material=0x7f04003a;
-        public static final int support_simple_spinner_dropdown_item=0x7f04003b;
-        public static final int toolbaricon=0x7f04003c;
+        public static final int na_drawer_profile_image=0x7f04002d;
+        public static final int nav_header=0x7f04002e;
+        public static final int notification_media_action=0x7f04002f;
+        public static final int notification_media_cancel_action=0x7f040030;
+        public static final int notification_template_big_media=0x7f040031;
+        public static final int notification_template_big_media_narrow=0x7f040032;
+        public static final int notification_template_lines=0x7f040033;
+        public static final int notification_template_media=0x7f040034;
+        public static final int notification_template_part_chronometer=0x7f040035;
+        public static final int notification_template_part_time=0x7f040036;
+        public static final int select_dialog_item_material=0x7f040037;
+        public static final int select_dialog_multichoice_material=0x7f040038;
+        public static final int select_dialog_singlechoice_material=0x7f040039;
+        public static final int support_simple_spinner_dropdown_item=0x7f04003a;
+        public static final int toolbaricon=0x7f04003b;
     }
     public static final class menu {
         public static final int menu=0x7f0d0000;
